b2daa7b7e1fd06411e70feb31efb6ac3d2dd66de
Nat Welch <nat@natwelch.com>
2012-05-12 12:35:54 -0700

    Enables etag support and turns all ' into " in site.rb

--- site.rb
+++ site.rb
@@ -2,22 +2,25 @@
 #
 # @author Nat Welch - https://github.com/icco
 
-require './models'
+require "./models"
 
 configure do
   set :sessions, true
 end
 
-get '/' do
+get "/" do
   erb :index
 end
 
-get '/data.csv' do
+get "/data.csv" do
   @stats = StatEntry.all
+
+  etag "data-#{StatEntry.max(:created_on)}"
+  content_type "text/csv"
   erb :"data.csv"
 end
 
-get '/style.css' do
-  content_type 'text/css', :charset => 'utf-8'
+get "/style.css" do
+  content_type "text/css", :charset => "utf-8"
   less :style
 end
