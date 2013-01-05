a8250d6d9ce3d9bdf3278885127f5abe9ce0bc4d
Nat Welch <nat@natwelch.com>
2013-01-05 00:46:50 -0800

    enable cache

--- app/app.rb
+++ app/app.rb
@@ -5,4 +5,8 @@ class Code < Padrino::Application
   register Padrino::Helpers
 
   enable :sessions
+
+  register Padrino::Cache
+  enable :caching
+  set :cache, Padrino::Cache::Store::Memory.new(50)
 end
