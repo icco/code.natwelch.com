17144e5f7589f051aa46f6fdb063de59b366293c
Nat Welch <nat@natwelch.com>
2014-11-10 07:38:57 -0800

    if exists

--- db/migrate/004_restart.rb
+++ db/migrate/004_restart.rb
@@ -1,9 +1,9 @@
 class Restart < ActiveRecord::Migration
   def change
     # This essentially defeats the purpose of having a migration.
-    drop_table :commits
-    drop_table :entries
-    drop_table :repos
+    drop_table :commits unless !ActiveRecord::Base.connection.table_exists? 'commits'
+    drop_table :entries unless !ActiveRecord::Base.connection.table_exists? 'entries'
+    drop_table :repos unless !ActiveRecord::Base.connection.table_exists? 'repos'
 
     create_table :commits do |t|
       t.string :repo
