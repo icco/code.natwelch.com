07f8fe528fc8ad83a112d9d9fa22b88314f4479b
Nat Welch <nat@natwelch.com>
2014-12-31 23:12:57 +0000

    Memcache!

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,14 @@ class Code < Padrino::Application
   enable :sessions
 
   register Padrino::Cache
-  set :cache, Padrino::Cache.new(:File, :dir => Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
   enable :caching
+  if Padrino.env.eql? :production
+    auth_pair = [
+      ENV['MEMCACHIER_PASSWORD'],
+      ENV['MEMCACHIER_USERNAME']
+    ]
+    set :cache, Padrino::Cache.new(:Memcached, ENV['MEMCACHIER_SERVERS'], :credentials => auth_pair, :exception_retry_limit => 1)
+  else
+    set :cache, Padrino::Cache.new(:File, :dir => Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
+  end
 end
